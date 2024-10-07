12e7f58c14ab035cbbea622cbbb01b3359772d7e
Alex <agoley@volexity.com>
2024-10-07 15:05:51 -0400

    test notification

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -21,15 +21,32 @@ const MyNotification = {
   tag: "unique-identifier",
 };
 
-function displayNotification() {
-  Notification.requestPermission().then(function (result) {
-    console.log(result); // granted!
-    new Notification("Permission Granted!", MyNotification);
-  });
+function notifyMe() {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification("Hi there!");
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      console.log("asking");
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        console.log("granted");
+        const notification = new Notification("Hi there!");
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
 }
 
 const button = document.getElementById("notifyMe");
 
-button.addEventListener("click", (e) => {
-  displayNotification();
-});
+button.addEventListener("click", notifyMe);
