b77865809fe75ff6f2274435758be4563fed88e1
Alex <agoley@volexity.com>
2024-10-12 15:09:50 -0400

    Update notification

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -1,9 +1,9 @@
 //The spec: https://notifications.spec.whatwg.org/
 const MyNotification = {
-  body: "Check out where I'll be speaking next",
+  body: "This is getting personal! Say hello :)",
   icon: "static/img/icons/icon-384x384.png",
   //Doesnt work on Apple!
-  image: "/static/img/speaking.jpg",
+  image: "/static/img/e-mail-symbol",
   //Sound is not supported... YET
   //   sound: "/static/sounds/BOTW_Fanfare_HeartContainer.wav",
   click_action: "/",
@@ -13,12 +13,12 @@ const MyNotification = {
   silent: false,
   // Vibration: Star Wars shamelessly taken from the awesome Peter Beverloo
   // https://tests.peter.sh/notification-generator/
-  vibrate: [
-    500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170,
-    40, 500,
-  ],
+  // vibrate: [
+  //   500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170,
+  //   40, 500,
+  // ],
   //Unique identifier for this notification
-  tag: "unique-identifier",
+  tag: new Date().getTime(),
 };
 
 function notifyMe() {
