bcd1a9a6340aff231ccce870d8967ce05952b3da
Alex <agoley@volexity.com>
2024-10-17 18:23:21 -0400

    Notification

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -6,7 +6,6 @@ const MyNotification = {
   image: "static/img/icons/icon-384x384.png",
   //Sound is not supported... YET
   //   sound: "/static/sounds/BOTW_Fanfare_HeartContainer.wav",
-  click_action: "mailto: ajgoley@gmail.com",
   //Keeps the notification displayed
   requireInteraction: false,
   //dont play a sound
@@ -23,21 +22,36 @@ const MyNotification = {
 
 const button = document.getElementById("notifyMe");
 
-function notifyMe() {
+function sendHug(notification) {
+  notification.onclick = function (event) {
+    event.preventDefault(); // prevent the browser from focusing the Notification's tab
+    window.open("mailto: ajgoley@gmail.com", "_blank");
+  };
+}
+
+function notifyVisitor() {
   if (!("Notification" in window)) {
     // Check if the browser supports notifications
     alert("This browser does not support desktop notification");
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    new Notification("Permission Granted!", MyNotification);
+    const notification = new Notification(
+      "Permission Granted!",
+      MyNotification,
+    );
+    sendHug(notification);
   } else if (Notification.permission !== "denied") {
     // We need to ask the user for permission
     Notification.requestPermission().then((permission) => {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        button.innerHTML = "DISABLE NOTIFICATIONS";
-        new Notification("Permission Granted!", MyNotification);
+        button.innerHTML = "FREE HUG 🤗";
+        const notification = new Notification(
+          "Permission Granted!",
+          MyNotification,
+        );
+        sendHug(notification);
       }
     });
   }
@@ -46,7 +60,7 @@ function notifyMe() {
   // want to be respectful there is no need to bother them anymore.
 }
 if (Notification.permission === "granted") {
-  button.innerHTML = "DISABLE NOTIFICATIONS";
+  button.innerHTML = "FREE HUG 🤗";
 }
 
-button.addEventListener("click", notifyMe);
+button.addEventListener("click", notifyVisitor);
